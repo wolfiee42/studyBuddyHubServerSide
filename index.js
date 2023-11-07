c45ca3d3ff8d787b04e6a2e3f1b1ecefb0052afd
wolfiee42 <saifalislam2022@gmail.com>
2023-11-07 12:52:46 +0600

    update section build with logic

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -40,10 +40,44 @@ async function run() {
             const assignment = req.body;
             const result = await assignmentCollection.insertOne(assignment)
             res.send(result);
-            console.log(result);
         })
-        app.get('/assignment', async(req, res)=>{
+
+        app.get('/assignment', async (req, res) => {
             const result = await assignmentCollection.find().toArray();
+            res.send(result);
+        })
+        // app.get('/assignment', async (req, res) => {
+        //     if(req.query.diffLevel){
+        //         query = {difficultyLevel : req.query.diffLevel}
+        //     }
+        //     const result = await assignmentCollection.find(query).toArray();
+        //     res.send(result);
+        //     console.log(result);
+        // })
+
+        app.get('/assignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await assignmentCollection.findOne(filter);
+            res.send(result);
+        })
+        app.put('/assignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const info = req.body;
+            const option = { upsert: true };
+            const updateInfo = {
+                $set: {
+                    name: info.name,
+                    title: info.title,
+                    desc: info.desc,
+                    marks: info.mark,
+                    image: info.image,
+                    difficultyLevel: info.diffLevel,
+                    startDate: info.startDate
+                }
+            }
+            const result = await assignmentCollection.updateOne(query, updateInfo, option);
             res.send(result);
             console.log(result);
         })
